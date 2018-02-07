ab6282161b3e532c39e81160b7e2b6082c93c29a
Sasha Patsel <sashapatsel@calvisitor-10-105-129-173.calvisitor.1918.berkeley.edu>
2018-02-07 12:26:43 -0800

    commute feature functional

--- assets/javascript/commute.js
+++ assets/javascript/commute.js
@@ -4,13 +4,29 @@
 var departLocation = "";
 var arriveLocation = "";
 
+
+function getCommuteTime(departLocation, arriveLocation) {
     var queryURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + departLocation + "&destinations=" + arriveLocation + "&key=AIzaSyBV2ruXz-bzml8rk9eIJJyQypcERMeSVhQ"
 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      
+
         console.log(response)
-        
+        var commuteTime = response.rows[0].elements[0].duration.text;
+        console.log(commuteTime)
        });
+
+}    
+  // Event handler for user clicking the select-weather button
+  $("#addRoute").on("click", function(event) {
+    // Preventing the button from trying to submit the form
+    event.preventDefault();
+    // Storing the weather name
+    var departLocation = $("#departInput").val().trim();
+    var arriveLocation = $("#arriveInput").val().trim();
+
+    // Running the searchBandsInTown function (passing in the weather as an argument)
+    getCommuteTime(departLocation, arriveLocation);
+  });
