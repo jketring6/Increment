faf31d2119bec85f12faf05f16fb8eae4054d915
jketring6 <jketring6@gmail.com>
2018-02-08 15:58:26 -0800

    Fixed background image sizing

--- assets/javascript/backgroundImages.js
+++ assets/javascript/backgroundImages.js
@@ -21,14 +21,16 @@ $.ajax({
 
   var backgroundImage = Math.floor(Math.random() * arr.length);
 
-  $('body').css('background','linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(' + arr[backgroundImage] + ')');
+  $('body').css('background','linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(' + arr[backgroundImage] + ') center');
+  $('body').css('background-size', 'cover');
 }, 
 function(errorObject) {
   var backupImg = ["assets/images/1.jpg", "assets/images/2.jpg", "assets/images/3.jpg", "assets/images/4.jpg", "assets/images/5.jpg", "assets/images/6.jpg", "assets/images/7.jpg", "assets/images/8.jpg", "assets/images/9.jpg", "assets/images/10.jpg", "assets/images/11.jpg", "assets/images/12.jpg", "assets/images/13.jpg", "assets/images/14.jpg"];
 
   var imgIndex = Math.floor(Math.random() * backupImg.length);
   tempImg = backupImg[imgIndex];
-  $('body').css('background', 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(' + tempImg + ')');
+  $('body').css('background', 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(' + tempImg + ') center');
+  $('body').css('background-size', 'cover');
   console.log("out");
 
 });
