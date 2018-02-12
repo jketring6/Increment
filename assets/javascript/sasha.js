5d9558f99245cba08992f5359abb62560fa1244c
Sasha Patsel <sashapatsel@calvisitor-10-105-129-61.calvisitor.1918.berkeley.edu>
2018-02-12 08:43:53 -0800

    add trending gif

--- assets/javascript/sasha.js
+++ assets/javascript/sasha.js
@@ -223,6 +223,40 @@ auth.onAuthStateChanged(firebaseUser => {
 //         console.log(val)
 //     });
 // });
+
+
+//=========================================================== Gifs ================================================================
+
+
+$("button").on("click", function() {
+
+
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=ICK0911MJ3OSyCVmYJsRtqgKZ0CzLbhW"
+
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // After data comes back from the request
+        .then(function(gifresponse) {
+
+            console.log(gifresponse);
+
+            var results = gifresponse.data
+
+            var imageUrl = results.images.fixed_height.url;
+        
+            var gifImage = $("<img>")
+
+            gifImage.attr("src", imageUrl);
+
+            $("#gifDiv").html(gifImage);
+
+            console.log("jeeksd");
+
+        });
+
+});
 //=========================================================== NEWS ================================================================
 
 
